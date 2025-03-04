dca05f384374dc53399573cd9d3ee7b66bf7ed52
pcbproj <pcbproj@yandex.ru>
2025-03-04 17:58:46 +0300

    next commit

--- send_repo_to_github.bat
+++ send_repo_to_github.bat
@@ -1,4 +1,4 @@
-git remote add origin https://github.com/pcbproj/FATFS_cast_try.git
+git remote add origin https://github.com/pcbproj/divider16x16.git
 git branch -M main
 git push -u origin main
 pause
